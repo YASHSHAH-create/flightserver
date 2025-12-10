a70d429f9d47afb3dda3fe984de6a13ce208c4db
YASH SHAH <coder@YASHs-MacBook-Air.local>
2025-12-11 03:22:21 +0530

    proxy tru

--- index.js
+++ index.js
@@ -34,7 +34,8 @@ app.use(session({
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback" // Make sure this matches Google Cloud Console
+    callbackURL: "/auth/google/callback", // Make sure this matches Google Cloud Console
+    proxy: true
 },
     async function (accessToken, refreshToken, profile, cb) {
         try {
